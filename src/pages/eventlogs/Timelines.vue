e2fa409f0013cf246b8fb4dcb3b6b8a70ea80287
xbgmsharp <xbgmsharp@gmail.com>
2025-04-12 21:17:57 +0000

    Add timeline page.

--- src/pages/eventlogs/Timelines.vue
+++ src/pages/eventlogs/Timelines.vue
@@ -1,29 +1,41 @@
+// prettier-ignore
 <template>
-  <div class="timelines">
-    <div class="grid grid-cols-12">
-      <div class="flex col-span-12">
-        <va-card no-padding>
-          <va-card-title>{{ t('timelines.verticalCentered') }}</va-card-title>
-          <va-card-content>
-            <va-timeline vertical centered style="min-width: 300px">
-              <va-timeline-item v-for="(item, key) in items" :key="key" color="danger" active>
-                <template #before>
-                  <span class="title title--danger va-timeline-item__text">
-                    {{ item.processed }}
-                  </span>
-                </template>
-                <template #after>
-                  <va-card stripe stripe-color="success">
-                    <va-card-title>{{ item.channel }}</va-card-title>
-                  </va-card>
-                </template>
-              </va-timeline-item>
-            </va-timeline>
-          </va-card-content>
-        </va-card>
-      </div>
-    </div>
-  </div>
+  <VaCard>
+    <VaCardTitle class="flex justify-between">
+      <h1 class="card-title text-secondary font-bold uppercase">Timeline</h1>
+      <h2>the last 500 events</h2>
+    </VaCardTitle>
+    <VaCardContent>
+      <table class="mt-4">
+        <tbody>
+          <VaTimelineItem v-for="(item, key) in items" :key="key" color="danger" active :date="item.fromnow">
+            <template v-if="item.channel === 'new_logbook'">
+              <RouterLink class="va-link link font-semibold" :to="{ name: 'log-map', params: { id: item.payload } }">{{
+                item.message
+              }}</RouterLink>
+              at {{ item.processed }}
+            </template>
+            <template v-else-if="item.channel === 'maplapse_video'">
+              <a :href="'/maplapse' + item.replay" target="_blank">
+                <span class="va-link link font-semibold">{{ item.message }}</span>
+                at {{ item.processed }}
+              </a>
+            </template>
+            <template v-else-if="item.channel === 'new_video'">
+              <a :href="'https://videos.openplotter.cloud/' + item.payload" target="_blank">
+                <span class="va-link link font-semibold">{{ item.message }}</span>
+                at {{ item.processed }}
+              </a>
+            </template>
+            <template v-else-if="item.channel === 'monitoring_offline' || item.channel === 'monitoring_online'">
+              <RouterLink class="va-link link font-semibold" to="/monitoring">{{ item.message }}</RouterLink>
+              at {{ item.processed }}
+            </template>
+          </VaTimelineItem>
+        </tbody>
+      </table>
+    </VaCardContent>
+  </VaCard>
 </template>
 
 <script setup>
@@ -31,12 +43,20 @@
   import { useI18n } from 'vue-i18n'
   import PostgSail from '../../services/api-client'
   import { dateFormatUTC } from '../../utils/dateFormatter.js'
-
+  import moment from 'moment'
+  import VaTimelineItem from '../../components/VaTimelineItem.vue'
   const { t } = useI18n()
 
   const isBusy = ref(false)
   const apiError = ref(null)
   const rowsData = ref([])
+  const messages = ref({
+    monitoring_offline: 'monitoring offline',
+    monitoring_online: 'monitoring online',
+    new_logbook: 'new logbook',
+    maplapse_video: 'replay video',
+    new_video: 'new video',
+  })
 
   const items = computed(() => {
     return Array.isArray(rowsData.value)
@@ -45,6 +65,9 @@
           processed: dateFormatUTC(row['processed']),
           payload: row['payload'],
           channel: row['channel'],
+          fromnow: row['processed'] ? moment(row['processed']).fromNow(true) : 'Pending',
+          message: row['channel'] ? messages.value[row['channel']] : 'unknown',
+          replay: row['channel'] === 'maplapse_video' ? row['payload'].substring(row['payload'].indexOf('?')) : null,
         }))
       : []
   })
@@ -55,11 +78,11 @@
     const api = new PostgSail()
     try {
       const response = await api.eventlogs()
-      console.log('Event Logs', response)
+      //console.log('Event Logs', response)
       if (Array.isArray(response)) {
         rowsData.value.splice(0, rowsData.value.length || [])
-        rowsData.value.push([...response].reverse())
-        console.log('Event Logs', rowsData.value)
+        rowsData.value.push(...response)
+        //console.log('Event Logs', rowsData.value)
       } else {
         throw { response }
       }
@@ -70,3 +93,12 @@
     }
   })
 </script>
+
+<style scoped>
+  .va-link {
+    color: var(--va-primary);
+  }
+  .va-link:hover {
+    color: var(--va-primary-dark);
+  }
+</style>
