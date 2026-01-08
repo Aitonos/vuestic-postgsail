b112f51bd0cee551cd15f77f504253f7d6b43234
xbgmsharp <xbgmsharp@gmail.com>
2026-01-08 07:17:11 +0000

    Update echarts linetimeseries, update default config and add theme support

--- src/components/echarts/linetimeseries.vue
+++ src/components/echarts/linetimeseries.vue
@@ -1,5 +1,7 @@
 <script setup>
   import { ref, computed } from 'vue'
+  import { dateFormatUTC } from '../../utils/dateFormatter.js'
+
   import VChart, { THEME_KEY } from 'vue-echarts'
   import { use } from 'echarts/core'
   import { LineChart } from 'echarts/charts'
@@ -13,68 +15,88 @@
       type: Array,
       required: true,
     },
+    theme: {
+      type: String,
+      default: THEME_KEY.LIGHT,
+    },
   })
 
-  // prettier-ignore
-  const defaultConfig = {
-  title: {
-    text: 'Boat Speed and Wind Speed Over Time',
-    left: 'center',
-    show: false
-  },
-  tooltip: {
-    trigger: 'axis'
-  },
-  legend: {
-    data: ['Boat Speed', 'Wind Speed'],
-    //top: '10%'
-  },
-  xAxis: {
-    type: 'category',
-    data: props.series.map(item => item[0]), // Time labels
-  },
-  yAxis: [
-    {
-      type: 'value',
-      name: 'Boat Speed',
-      position: 'left'
+  const defaultConfig = ref({
+    title: {
+      text: 'Boat Speed and Wind Speed Over Time',
+      left: 'center',
+      show: false,
     },
-    {
-      type: 'value',
-      name: 'Wind Speed',
-      position: 'right'
-    }
-  ],
-  series: [
-    {
-      name: 'Boat Speed',
-      type: 'line',
-      yAxisIndex: 0,
-      data: props.series.map(item => item[1]), // Boat speed data
-      smooth: true,
-      lineStyle: {
-        color: 'blue'
-      }
+    tooltip: {
+      trigger: 'axis',
+      formatter: function (params) {
+        // params is an array of series data at the hovered x-axis value
+        const time = params[0].axisValue // x-axis label
+        let tooltipText = `<strong>${dateFormatUTC(time)}</strong><br/>`
+
+        params.forEach((item) => {
+          const value = Number(item.data).toFixed(2) // format to 2 decimals
+          const unit = item.seriesName === 'Boat Speed' ? 'knots' : 'knots' // or change if units differ
+          tooltipText += `
+          <span style="display:inline-block;margin-right:4px;
+            border-radius:50%;width:10px;height:10px;
+            background-color:${item.color};"></span>
+          ${item.seriesName}: ${value} ${unit}<br/>`
+        })
+
+        return tooltipText
+      },
     },
-    {
-      name: 'Wind Speed',
-      type: 'line',
-      yAxisIndex: 1,
-      data: props.series.map(item => item[2]), // Wind speed data
-      smooth: true,
-      lineStyle: {
-        color: 'red'
-      }
-    }
-  ]
-}
+    legend: {
+      data: ['Boat Speed', 'Wind Speed'],
+      //top: '10%'
+    },
+    xAxis: {
+      type: 'category',
+      data: props.series.map((item) => item[0]), // Time labels
+    },
+    yAxis: [
+      {
+        type: 'value',
+        //name: 'Boat Speed',
+        position: 'left',
+      },
+      {
+        type: 'value',
+        //name: 'Wind Speed',
+        position: 'right',
+      },
+    ],
+    series: [
+      {
+        name: 'Boat Speed',
+        type: 'line',
+        yAxisIndex: 0,
+        data: props.series.map((item) => item[1]), // Boat speed data
+        smooth: true,
+        lineStyle: {
+          color: 'blue',
+        },
+      },
+      {
+        name: 'Wind Speed',
+        type: 'line',
+        yAxisIndex: 1,
+        data: props.series.map((item) => item[2]), // Wind speed data
+        smooth: true,
+        lineStyle: {
+          color: 'red',
+        },
+      },
+    ],
+  })
 
   const chartOptions = computed(() => {
-    const localoptions = { ...defaultConfig }
-    if (Array.isArray(props.series) && props.series.length > 0) {
-      //localoptions.dataset.source = props.series || []
-    }
-    return localoptions
+    //const localoptions = { ...defaultConfig }
+    //if (Array.isArray(props.series) && props.series.length > 0) {
+    //  //localoptions.dataset.source = props.series || []
+    //}
+    return defaultConfig.value
   })
   const themeOption = computed(() => {
     return props.theme || THEME_KEY.LIGHT
@@ -83,7 +105,7 @@
 
 <template>
   <div id="echarts">
-    <v-chart :option="chartOptions" autoresize />
+    <v-chart :option="chartOptions" :theme="themeOption" autoresize />
   </div>
 </template>
 
