f26076edd124a4821f2869ed0f3541117289d265
xbgmsharp <xbgmsharp@gmail.com>
2026-03-08 13:48:20 +0000

    Update Settings page.

--- src/pages/profile/Settings.vue
+++ src/pages/profile/Settings.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="flex flex-col space-y-6 md:space-y-4 text-black dark:text-white">
-    <h1 class="page-title">{{ t('profile.title') }}</h1>
-    <va-tabs v-model="activeTabName" grow>
-      <template #tabs>
-        <va-tab name="OverviewTab">
-          {{ t('profile.tabs.overview.title') }}
-        </va-tab>
-        <va-tab name="NotificationsTab">
-          {{ t('profile.tabs.notifications.title') }}
-        </va-tab>
-      </template>
-    </va-tabs>
-    <va-separator />
-    <div class="max-w-full overflow-x-hidden">
-      <component :is="tabs[activeTabName]" @submit="submit" />
-    </div>
+    <va-card>
+      <va-card-title>
+        {{ t('profile.title') }}
+      </va-card-title>
+      <va-card-content>
+        <va-tabs v-model="activeTabName" grow>
+          <template #tabs>
+            <va-tab name="OverviewTab">
+              {{ t('profile.tabs.overview.title') }}
+            </va-tab>
+            <va-tab name="NotificationsTab">
+              {{ t('profile.tabs.notifications.title') }}
+            </va-tab>
+          </template>
+        </va-tabs>
+        <va-separator />
+        <div class="max-w-full overflow-x-hidden">
+          <component :is="tabs[activeTabName]" @submit="submit" />
+        </div>
+      </va-card-content>
+    </va-card>
   </div>
 </template>
 
